5f7b9e21f7007e80bb6dd3d77853f5cd57b830bd
Harun Veli KAYSILI <harun.kaysili@dygdigital.com>
2022-07-19 14:49:01 +0300

    useRef & forwardRef kullanım örnekleri

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React - useRef & forwardRef
-`Bu çalışmada ***useRef*** **&** ***forwardRef*** kullanımları ile alakalı detaylı örnekleri bulabilirsiniz`
+`Bu çalışmada useRef  & forwardRef kullanımları ile alakalı detaylı örnekleri bulabilirsiniz`
+
+useRef     => jsx elemanlarını ref'lemek için kullanılır.
+forwardRef => componentleri reflemek için kullanılır. 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import {useRef, forwardRef} from 'react';
 import './App.css';
 
+// ref aktarımı --> component'e
+const Input = forwardRef((props, ref)  => {
+  return <input ref={ref} type="text" {...props}/>
+}) 
+
 function App() {
+  const inputRefFirst = useRef();
+  const inputRefSecond = useRef();
+
+  const focusInput_useRef = () => { 
+    inputRefFirst.current.focus()
+  }
+  const focusInput_forwardRef = () => { 
+    inputRefSecond.current.focus()
+  }
+
   return (
     <div className="App">
         <h3>useRef & forwardRef Usage</h3>
         <hr/>
-        
+        <div style={{display:"flex", gap:"5px", justifyContent:"center", alignItems:"center"}}> 
+          {/*    ******* useRef Kullanımı ********* */}
+          <input type="text" ref={inputRefFirst}/>
+          <button onClick={focusInput_useRef}>Focusla</button>  
+
+          {/*    ******* forwardRef Kullanımı ********* */}
+          <Input ref={inputRefSecond} /> 
+          <button onClick={focusInput_forwardRef}>Focusla</button>
+        </div>
     </div>
   );
 }
